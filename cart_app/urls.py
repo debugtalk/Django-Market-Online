0fd0c6c55919c9d6644cc0ce2cc2602988b857f2
leolee <leo@Debian7-Leo>
2014-11-21 23:50:55 +0800

    delete cart_app in remote Github

--- cart_app/urls.py
+++
@@ -1,8 +0,0 @@
-#encoding:utf-8
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('cartapp.views',
-    url(r'^view/$', 'view_cart', name='view_cart'),
-    url(r'^add/(?P<id>\d+)/$', 'add_to_cart', name='add_to_cart'),
-    url(r'^clean/$', 'clean_cart', name='clean_cart'),
-)
